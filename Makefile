f70e2b0ad63ea5409f685068dbdd1ba2b09deb70
wang wenhai <3143036182@qq.com>
2025-09-04 16:39:23 +0800

    finsh symlink

--- Makefile
+++ Makefile
@@ -245,7 +245,8 @@ endif
 
 ifeq ($(LAB),fs)
 UPROGS += \
-	$U/_bigfile
+	$U/_bigfile\
+	$U/_symlinktest
 endif
 
 
--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -296,7 +296,6 @@ sys_open(void)
     return -1;
 
   begin_op();
-
   if(omode & O_CREATE){
     ip = create(path, T_FILE, 0, 0);
     if(ip == 0){
@@ -316,6 +315,41 @@ sys_open(void)
     }
   }
 
+if (ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)) {
+    int tolerate = 10;
+    int len = 0;
+    char target[MAXPATH];
+    while (ip->type == T_SYMLINK && tolerate > 0) {
+      if (readi(ip, 0, (uint64)&len, 0, sizeof(len)) != sizeof(len)) {
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
+      if (len <= 0 || len >= MAXPATH - 1) {
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
+      if (readi(ip, 0, (uint64)target, sizeof(len), len+1) != len+1) {
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
+      iunlockput(ip);
+      if((ip = namei(target)) == 0) {
+        end_op();
+        return -1;
+      }
+      ilock(ip);
+      tolerate--;
+    }
+    // cycle symlink is not allowed
+    if (tolerate == 0) {
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
+  }
   if(ip->type == T_DEVICE && (ip->major < 0 || ip->major >= NDEV)){
     iunlockput(ip);
     end_op();
@@ -482,5 +516,47 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+uint64
+sys_symlink(void)
+{
+  char target[MAXPATH];
+  char path[MAXPATH];
+  struct inode *ip;
+  int n;
+
+  if((n = argstr(0, target, MAXPATH)) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+
+  ip = create(path, T_SYMLINK, 0, 0);
+  if(ip == 0){
+      end_op();
+      return -1;
+  }
+  // ilock(ip);
+  // create a new symlink, return with a locked inode
+  int len = strlen(target);
+  if(len < 0 || len >= MAXPATH -1){
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+  if(writei(ip, 0, (uint64)&len, 0, sizeof(len)) != sizeof(len)){
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+
+  if(writei(ip, 0, (uint64)target, sizeof(len), len+1) != len+1){
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+
+  iunlockput(ip);
+  end_op();
   return 0;
 }
